b8728e207cc272c0507fd163126b7844c5aad052
toeiisk <42915021+toeiisk@users.noreply.github.com>
2020-11-24 10:26:18 +0700

    uppp

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn Shipping.wsgi --log-file -
-release: python manage.py makemigrations
-release: python manage.py migrate
--- Services/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Carrier)
-admin.site.register(OrderItem)
-admin.site.register(OrderDescription)
